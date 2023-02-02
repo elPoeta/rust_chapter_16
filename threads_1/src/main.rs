a6425d2848aa5f7e0b715559812672f86a78fbfa
Leonardo Tosetto <leonardo.a.tosetto@gmail.com>
2023-02-02 08:48:59 -0300

    using threads to run code simultaneously

--- threads_1/src/main.rs
+++ threads_1/src/main.rs
@@ -1,5 +1,53 @@
 #![allow(unused)]
 
+use std::thread;
+use std::time::Duration;
+
 fn main() {
-    println!("Hello, world!");
+    {
+        thread::spawn(|| {
+            for i in 1..10 {
+                println!("hi number {} from the spawned thread!", i);
+                thread::sleep(Duration::from_millis(1));
+            }
+        });
+
+        for i in 1..5 {
+            println!("hi number {} from the main thread!", i);
+            thread::sleep(Duration::from_millis(1));
+        }
+    }
+
+    {
+        println!("****************************************************");
+
+        let handle = thread::spawn(|| {
+            for i in 1..10 {
+                println!("hi number {} from the spawned thread!", i);
+                thread::sleep(Duration::from_millis(1));
+            }
+        });
+
+        // where join is called, can affect whether or not your threads run at the same time.
+        // handle.join().unwrap();
+
+        for i in 1..5 {
+            println!("hi number {} from the main thread!", i);
+            thread::sleep(Duration::from_millis(1));
+        }
+
+        handle.join().unwrap();
+    }
+
+    {
+        println!("**************************************************");
+
+        let v = vec![1, 2, 3];
+
+        let handle = thread::spawn(move || {
+            println!("Here's a vector: {:?}", v);
+        });
+
+        handle.join().unwrap();
+    }
 }
